6a9ba3612ae9fd09eaee825f97142e5db7c26b06
Vladimir <vl.kutepov@gmail.com>
2025-01-19 22:47:00 +1000

    feat: complete task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(filePath, 'utf-8');
+
+stream.on('data', (data) => {
+  process.stdout.write(data);
+});
